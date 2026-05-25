59da7a5a605fd0ea2228b90979c0eda80818965c
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 02:00:52 -0500

    add book properties w/o derived methods

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
-
 public class Book {
+
+    //add properties
+    int Id;
+    String isbn;
+    String title;
+    boolean isCheckedOut;
+    String checkedOutTo;
+
+    //add constructor
+
+    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+        Id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    //add get/ set
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+
+    // add methods derived
+
 }
